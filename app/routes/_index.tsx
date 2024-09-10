85c26fd6781eee0a19547e3ba60a96a982536148
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-09-10 16:30:46 +0100

    edited readme

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,13 +1,12 @@
-import { Flex, Heading, Text } from "@radix-ui/themes";
+import { Flex, Heading } from "@radix-ui/themes";
 import type { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Studioso" },
+    { title: "Blueprint" },
     {
       name: "description",
-      content: "Create,Edit and Collaborate on PDF Files with Ease",
+      content: "The best way to get a remix project started",
     },
   ];
 };
@@ -15,16 +14,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <Flex direction="column" className="font-sans bg-zinc-50 w-full h-screen">
-      <Flex className="px-3 py-3" align="center" justify="between">
-        <Link to="/">
-          <Text size="3" weight="medium">
-            Studioso
-          </Text>
-        </Link>
-      </Flex>
-      <Flex direction="column" align="center" justify="center">
-        <Heading size="8">Studioso</Heading>
-      </Flex>
+      <Heading>Blueprint</Heading>
     </Flex>
   );
 }
